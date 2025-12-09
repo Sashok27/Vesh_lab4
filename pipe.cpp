01eda20636e202390950937c9512ffc20a08420a
Alex Vesh <veshkurtsevalex@gmail.com>
2025-12-09 22:27:14 +0300

    добавляем новые поля

--- pipe.cpp
+++ pipe.cpp
@@ -1,6 +1,8 @@
 #include "pipe.h"
 #include "shablones.h"
 #include <iostream>
+#include <cmath>      // для sqrt pow
+#include <limits>     // для numeric_limits
 using namespace std; 
 
 void Pipe::input() {
@@ -34,4 +36,30 @@ std::istream& operator>>(std::istream& is, Pipe& pipe) {
     getline(is, pipe.name);
     is >> pipe.length >> pipe.diameter >> pipe.repair;
     return is;
+}
+
+
+double Pipe::getProductivity() const {
+    if (repair) return 0.0;  // Если в ремонте - пропускная способность 0
+    
+    //диаметр в мм, переводим в метры
+    double d_m = diameter / 1000.0;
+    //длина в км, переводим в метры
+    double l_m = length * 1000.0;
+    
+    //проверка деления на ноль
+    if (l_m <= 0) return 0.0;
+    
+    // c * sqrt(d^5 / l), c = 0.0384
+    double productivity = 0.0384 * sqrt(pow(d_m, 5) / l_m);
+    
+    //поправочный коэффициент для удобства
+    productivity *= 1000;
+    
+    return productivity;
+}
+
+double Pipe::getWeight() const {
+    if (repair) return std::numeric_limits<double>::infinity();  // Бесконечный вес при ремонте
+    return length;  // Вес = длина в км
 }
